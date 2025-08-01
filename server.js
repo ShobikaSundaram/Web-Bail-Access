93257b9e459aec0156b311a6ec1b8aee73720e47
ShobikaSundaram <shobikasundaram18@gmail.com>
2025-08-01 21:55:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ app.get('/api/judge', (req, res) => {
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => {
   console.log('MongoDB connected');
